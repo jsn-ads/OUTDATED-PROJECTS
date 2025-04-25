4a84728cff4c47dfd887d41b947969643c73ef54
Neto Home <jsn.ads@gmail.com>
2025-04-25 16:20:22 -0300

    PHP OLD PP OO - configurações

--- 04.PHP (old)/11-DevsBook-POO/config.php
+++ 04.PHP (old)/11-DevsBook-POO/config.php
@@ -1,14 +1,12 @@
 <?php
 
-    session_start();
+session_start();
 
-    $base = 'http://localhost/PROJETOS/NEWPHP/11-DevsBook-POO';
+$base = 'http://localhost/PROJETOS/OUTDATED-PROJECTS/04.PHP (old)/11-DevsBook-POO';
 
-    $db_name = 'devsbookpoo';
-    $db_host = '127.0.0.1';
-    $db_user = 'adm@adm';
-    $db_pass = '229536';
+$db_name = 'devsbookpoo';
+$db_host = '127.0.0.1';
+$db_user = 'adm@adm';
+$db_pass = '229536';
 
-    $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
-
-?>
+$pdo = new PDO("mysql:dbname=" . $db_name . ";host=" . $db_host, $db_user, $db_pass);
--- 04.PHP (old)/11-DevsBook-POO/configuracoes_action.php
+++ 04.PHP (old)/11-DevsBook-POO/configuracoes_action.php
@@ -60,9 +60,131 @@ if ($name && $email) {
     $userInfo->work = $work;
     $userInfo->birthdate = $birthdate;
 
+    // AVATAR
+
+    if (isset($_FILES['avatar']) && !empty($_FILES['avatar']['tmp_name'])) {
+
+        $newAvatar = $_FILES['avatar'];
+
+        if (in_array($newAvatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $avatarWidth = 200;
+            $avatarHeight = 200;
+
+            list($widthOrig, $heightOrig) = getimagesize($newAvatar['tmp_name']);
+
+            $radio = $widthOrig / $heightOrig;
+            $newWidth = $avatarWidth;
+            $newHeight = $newWidth / $radio;
+
+            if ($newHeight < $avatarHeight) {
+                $newHeight = $avatarHeight;
+                $newWidth = $newHeight * $radio;
+            }
+
+            $x = $avatarWidth - $newWidth;
+            $y = $avatarHeight - $newHeight;
+            $x = $x < 0 ? $x / 2 : $x;
+            $y = $y < 0 ? $y / 2 : $x;
+
+            $finalImage = imagecreatetruecolor($avatarWidth, $avatarHeight);
+
+            switch ($newAvatar['type']) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                    $image = imagecreatefromjpeg($newAvatar['tmp_name']);
+                    break;
+
+                case 'image/png':
+                    $image = imagecreatefrompng($newAvatar['tmp_name']);
+                    break;
+            }
+
+            imagecopyresampled(
+                $finalImage,
+                $image,
+                $x,
+                $y,
+                0,
+                0,
+                $newWidth,
+                $newHeight,
+                $widthOrig,
+                $heightOrig
+            );
+
+            $avatarName = md5(time() . rand(0, 9999)) . '.jpg';
+
+            imagejpeg($finalImage, './media/avatars/' . $avatarName, 100);
+
+            $userInfo->avatar = $avatarName;
+        }
+    }
+
+    // COVER
+
+    if (isset($_FILES['cover']) && !empty($_FILES['cover']['tmp_name'])) {
+
+        $newCover = $_FILES['cover'];
+
+        if (in_array($newCover['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $coverWidth = 850;
+            $coverHeight = 310;
+
+            list($widthOrig, $heightOrig) = getimagesize($newCover['tmp_name']);
+
+            $radio = $widthOrig / $heightOrig;
+            $newWidth = $coverWidth;
+            $newHeight = $newWidth / $radio;
+
+            if ($newHeight < $coverHeight) {
+                $newHeight = $coverHeight;
+                $newWidth = $newHeight * $radio;
+            }
+
+            $x = $coverWidth - $newWidth;
+            $y = $coverHeight - $newHeight;
+            $x = $x < 0 ? $x / 2 : $x;
+            $y = $y < 0 ? $y / 2 : $x;
+
+            $finalImage = imagecreatetruecolor($coverWidth, $coverHeight);
+
+            switch ($newCover['type']) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                    $image = imagecreatefromjpeg($newCover['tmp_name']);
+                    break;
+
+                case 'image/png':
+                    $image = imagecreatefrompng($newCover['tmp_name']);
+                    break;
+            }
+
+            imagecopyresampled(
+                $finalImage,
+                $image,
+                $x,
+                $y,
+                0,
+                0,
+                $newWidth,
+                $newHeight,
+                $widthOrig,
+                $heightOrig
+            );
+
+            $coverName = md5(time() . rand(0, 9999)) . '.jpg';
+
+            imagejpeg($finalImage, './media/covers/' . $coverName, 100);
+
+            $userInfo->cover = $coverName;
+        }
+    }
+
+
     $userDao->update($userInfo);
 
     header("Location:" . $base . "/configuracoes.php");
+
     exit;
 }
 
